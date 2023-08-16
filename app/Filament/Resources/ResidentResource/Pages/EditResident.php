abbb9d3506358d27a3c7e56c68ca8e81138a1d87
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-08-17 03:32:28 +0800

    Update EditResident.php

--- app/Filament/Resources/ResidentResource/Pages/EditResident.php
+++ app/Filament/Resources/ResidentResource/Pages/EditResident.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\ResidentResource\Pages;
 
 use App\Filament\Resources\ResidentResource;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
 
 class EditResident extends EditRecord
@@ -14,6 +15,15 @@ class EditResident extends EditRecord
     {
         return [
             Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getFormActions(): array
+    {
+        return [
+            Action::make('go_back')
+                ->url($this->previousUrl ?? static::getResource()::getUrl())
+                ->color('gray')
         ];
     }
 }
