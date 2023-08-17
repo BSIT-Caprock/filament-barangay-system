d70c8ea480d0367383046407f7a04d4f7bb7c65e
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-08-17 12:07:22 +0800

    requests models

---
+++ app/Models/IndigencyCertificate.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+class IndigencyCertificate extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'resident_id',
+    ];
+
+    public function resident()
+    {
+        return $this->belongsTo(Resident::class);
+    }
+
+    public function receipt() : MorphOne
+    {
+        return $this->morphOne(Receipt::class, 'receiptable');
+    }
+}
